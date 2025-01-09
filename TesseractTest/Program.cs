c7ab5a98cc2c3517ac0b6d7893dd4dc40f49bbfa
Adedamola Adeyemi <adedamolaiadeyemi@gmail.com>
2025-01-09 09:18:03 +0100

    Adding Tesseract functional folder

--- TesseractTest/Program.cs
+++ TesseractTest/Program.cs
@@ -25,15 +25,23 @@ namespace TesseractTest
         {
             try
             {
+
+                string currentDirectory = Environment.CurrentDirectory;
+                Console.WriteLine($"Current Directory (Environment.CurrentDirectory): {currentDirectory}");
+
+
+
                 // Specify the tessdata path (update this path if Tesseract is installed in a different directory)
-                string tessDataPath = @"C:\Program Files\Tesseract-OCR\tessdata";
+                string tessDataPath = @"Tesseract-OCR\tessdata";
+                //string tessDataPath = @"C:\Program Files\Tesseract-OCR\tessdata";
                 //string tessDataPath = "/usr/share/tesseract-ocr/4.00/tessdata/";
 
                 // Initialize the Tesseract engine
                 using (var engine = new TesseractEngine(tessDataPath, "eng", EngineMode.Default))
                 {
                     // Load the test image (update the path to your test image)
-                    string imagePath = @"C:\Program Files\Tesseract-OCR\tessdata\test_image.jpg";
+                    string imagePath = @"Tesseract-OCR\tessdata\test_image.jpg";
+                   // string imagePath = @"C:\Program Files\Tesseract-OCR\tessdata\test_image.jpg";
                     using (var img = Pix.LoadFromFile(imagePath))
                     {
                         // Perform OCR
