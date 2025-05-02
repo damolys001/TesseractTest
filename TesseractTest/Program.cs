0f5bb622b1687901987654599fce2bca513c45e9
Adedamola Adeyemi <Adedamolaiadeyemi@gmail.com>
2025-05-02 13:25:14 +0100

    Update Program.cs

--- TesseractTest/Program.cs
+++ TesseractTest/Program.cs
@@ -1,15 +1,4 @@
 ﻿//namespace TesseractTest
-//{
-//    internal class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello, World!");
-//        }
-//    }
-//}
-
-
 
 
 using System;
